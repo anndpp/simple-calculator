69688e4d2b1fd1b1a3a1cfe59fdebae6729c0630
anndpp <93894461+anndpp@users.noreply.github.com>
2023-06-23 16:32:45 +1000

    simple calculator done

--- calculator.py
+++ calculator.py
@@ -1,21 +1,35 @@
-operation = ["+", "-", "*", "/"]
-
 def question():
     print()
     print("Please select operation -")
     print("1. Addition \n2. Substraction \n3. Multiplication \n4. Division")
 
-def add():
-    pass
+    operation_input = int(input("Select operation 1-4: "))
 
-def substract():
-    pass
+    num1 = int(input("Enter the 1st number: "))
+    num2 = int(input("Enter the 2nd number: "))
+    
+    if operation_input == 1:
+        print(f"{num1} + {num2} =", add(num1, num2))
+    elif operation_input == 2:
+        print(f"{num1} - {num2} =", substract(num1, num2))
+    elif operation_input == 3:
+        print(f"{num1} * {num2} =", multiply(num1, num2))
+    elif operation_input == 4:
+        print(f"{num1} / {num2} =", divide(num1, num2))
+    else:
+        print("Invalid operator")
 
-def multiply():
-    pass
+def add(num1, num2):
+    return num1 + num2
 
-def divide():
-    pass
+def substract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
 
 if __name__ == "__main__":
     question()
