088c0e6839233c8811b88d5a9cd39a8228137299
anndpp <93894461+anndpp@users.noreply.github.com>
2023-06-23 16:20:06 +1000

    basic functions

---
+++ calculator.py
@@ -0,0 +1,22 @@
+operation = ["+", "-", "*", "/"]
+
+def question():
+    print()
+    print("Please select operation -")
+    print("1. Addition \n2. Substraction \n3. Multiplication \n4. Division")
+
+def add():
+    pass
+
+def substract():
+    pass
+
+def multiply():
+    pass
+
+def divide():
+    pass
+
+if __name__ == "__main__":
+    question()
+
